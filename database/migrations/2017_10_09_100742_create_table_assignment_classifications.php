a579db7238383d16c0596c57597c67d5462f7f80
hasee <1147913074@qq.com>
2017-10-16 22:09:55 +0800

    添加  类 委托分类  以及对应的表migrate

---
+++ database/migrations/2017_10_09_100742_create_table_assignment_classifications.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableAssignmentClassifications extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('assignment_classifications', function (Blueprint $table) {
+            $table->increments('id')->index();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('assignment_classifications');
+    }
+}
