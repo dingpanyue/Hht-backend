34dbba80a6431d3a4a36304fff11a0555cf13e6c
hasee <1147913074@qq.com>
2018-01-23 22:31:14 +0800

    测试

--- app/Services/AssignmentService.php
+++ app/Services/AssignmentService.php
@@ -103,7 +103,7 @@ class AssignmentService
                     $assignments[$k]->distance = $distance;
                 }
             }
-            dd($assignments->items);
+            dd($assignments);
             $assignments = array_values($assignments);
         } else {
             $assignments = $assignments->orderBy($orderBy, $order)->paginate('20');
