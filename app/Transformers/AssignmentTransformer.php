18d2bac964a7b01db78799b6459cd437a4394e30
hasee <1147913074@qq.com>
2018-02-05 11:01:56 +0800

    微信提现到银行卡接口

--- app/Transformers/AssignmentTransformer.php
+++ app/Transformers/AssignmentTransformer.php
@@ -17,7 +17,7 @@ class AssignmentTransformer
 
         $statuses = [
             Assignment::STATUS_UNPAID => '未支付',
-            Assignment::STATUS_WAIT_ACCEPT => '待接受',
+            Assignment::STATUS_WAIT_ACCEPT => '待选择服务方',
             Assignment::STATUS_ADAPTED => '已采纳',
             Assignment::STATUS_CANCELED => '已取消',
             Assignment::STATUS_FINISHED => '已完成',
