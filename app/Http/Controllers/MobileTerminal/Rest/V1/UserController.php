1ebd95fdba1d8e13e6692c4f0177a21d35869b52
hasee <1147913074@qq.com>
2018-04-01 17:19:48 +0800

    添加redis扩展

--- app/Http/Controllers/MobileTerminal/Rest/V1/UserController.php
+++ app/Http/Controllers/MobileTerminal/Rest/V1/UserController.php
@@ -662,7 +662,7 @@ class UserController extends BaseController
             return self::parametersIllegal($validator->messages()->first());
         }
 
-        Redis::set($user->id , [$user->id => [$inputs['lng'], $inputs['lat']]]);
+        Redis::set($user->id, serialize([$user->id => [$inputs['lng'], $inputs['lat']]]));
 
         dd(Redis::get($user->id));
     }
